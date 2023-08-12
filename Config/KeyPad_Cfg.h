160a4db10515307191819780158df37e94e0a0ef
Omar Khaled Ahmed <136479749+OmarKhaledKhalil@users.noreply.github.com>
2023-08-12 18:20:58 +0300

    Add files via upload

---
+++ Config/KeyPad_Cfg.h
@@ -0,0 +1,14 @@
+#ifndef KEYPAD_CFG_H_
+#define KEYPAD_CFG_H_
+
+#define FIRST_OUPUT PINB4
+#define FIRST_INPUT PIND4
+
+#define ROWS 4
+#define COLS 4
+
+extern const u8 KeysArray[ROWS][COLS];
+
+
+
+#endif
